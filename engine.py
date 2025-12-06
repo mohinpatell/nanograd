0abe036e21cc5171341b9719f691051e97e88525
Mohin Patel <mohinpatel1728@gmail.com>
2025-12-06 14:08:00 -0800

    implement Value class with add and mul

--- engine.py
+++ engine.py
@@ -4,17 +4,37 @@ class Value:
     def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
-        # internal variables for autograd graph
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
 
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
+
     def __repr__(self):
         return f"Value(data={self.data})"
 
+    # so we can do 2 + Value(3) not just Value(3) + 2
+    def __radd__(self, other):
+        return self + other
 
-# quick test
+    def __rmul__(self, other):
+        return self * other
+
+
 if __name__ == '__main__':
     a = Value(2.0)
-    b = Value(3.0)
-    print(a, b)
+    b = Value(-3.0)
+    c = a * b + Value(10.0)
+    print(f"a = {a}")
+    print(f"b = {b}")
+    print(f"c = a*b + 10 = {c}")
+    print(f"c._prev = {c._prev}")
+    print(f"c._op = {c._op}")

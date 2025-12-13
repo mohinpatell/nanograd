791eb1814bfeb9fb51ebed980a073abc39601886
Mohin Patel <mohinpatel1728@gmail.com>
2025-12-12 22:26:00 -0800

    tensor add and mul with grad tracking

--- tensor.py
+++ tensor.py
@@ -5,7 +5,7 @@ class Tensor:
     """A tensor with automatic differentiation support, backed by numpy."""
 
     def __init__(self, data, _children=(), _op='', requires_grad=False):
-        if isinstance(data, (int, float)):
+        if isinstance(data, (int, float, np.floating, np.integer)):
             data = np.array(data, dtype=np.float32)
         elif isinstance(data, list):
             data = np.array(data, dtype=np.float32)
@@ -32,6 +32,45 @@ class Tensor:
     def dtype(self):
         return self.data.dtype
 
+    def _make_tensor(self, other):
+        if isinstance(other, Tensor):
+            return other
+        return Tensor(other)
+
+    def __add__(self, other):
+        other = self._make_tensor(other)
+        out = Tensor(self.data + other.data, (self, other), '+',
+                     requires_grad=self.requires_grad or other.requires_grad)
+
+        def _backward():
+            if self.requires_grad:
+                self.grad += out.grad
+            if other.requires_grad:
+                other.grad += out.grad
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        other = self._make_tensor(other)
+        out = Tensor(self.data * other.data, (self, other), '*',
+                     requires_grad=self.requires_grad or other.requires_grad)
+
+        def _backward():
+            if self.requires_grad:
+                self.grad += other.data * out.grad
+            if other.requires_grad:
+                other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __radd__(self, other):
+        return self + other
+
+    def __rmul__(self, other):
+        return self * other
+
     def __repr__(self):
         return f"Tensor({self.data}, requires_grad={self.requires_grad})"
 
@@ -54,14 +93,36 @@ class Tensor:
 
 
 if __name__ == '__main__':
-    # basic creation tests
-    a = Tensor([1, 2, 3])
-    print(f"a = {a}")
-    print(f"a.shape = {a.shape}")
+    # element-wise add
+    a = Tensor([1, 2, 3], requires_grad=True)
+    b = Tensor([4, 5, 6], requires_grad=True)
+    c = a + b
+    s = Tensor(c.data.sum(), (c,), 'sum', requires_grad=True)
 
-    b = Tensor([[1, 2], [3, 4]], requires_grad=True)
-    print(f"b = {b}")
-    print(f"b.grad = {b.grad}")
+    # manually wire up the sum backward for now
+    def _sum_backward():
+        c.grad = np.ones_like(c.data) * s.grad if c.requires_grad else None
+    s._backward = _sum_backward
+    c.requires_grad = True
+    c.grad = np.zeros_like(c.data)
 
-    c = Tensor(5.0)
-    print(f"scalar: {c}, shape: {c.shape}")
+    s.backward()
+    print(f"a + b = {c.data}")
+    print(f"a.grad = {a.grad}")  # should be [1, 1, 1]
+    print(f"b.grad = {b.grad}")  # should be [1, 1, 1]
+
+    # element-wise mul
+    a = Tensor([2, 3], requires_grad=True)
+    b = Tensor([4, 5], requires_grad=True)
+    c = a * b  # [8, 15]
+    s = Tensor(c.data.sum(), (c,), 'sum', requires_grad=True)
+    def _sum_backward2():
+        c.grad = np.ones_like(c.data) * s.grad
+    s._backward = _sum_backward2
+    c.requires_grad = True
+    c.grad = np.zeros_like(c.data)
+
+    s.backward()
+    print(f"\na * b = {c.data}")
+    print(f"a.grad = {a.grad}")  # should be [4, 5]
+    print(f"b.grad = {b.grad}")  # should be [2, 3]
